15f658c866fdf6aead6edbbe9263ed1a7e944194
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-22 14:03:00 +0200

    adding some styling

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ImagesList.css';
 
 const ImagesList = ({ images}) => {
     const displayImages = images.map(img =>{
     return(<img src={img.urls.thumb} key={img.id} alt={img.alt_description}/>)
     });
-    return(<div>
+    return(<div className="images-list">
     {displayImages}</div>);
 }
 
