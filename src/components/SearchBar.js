3a7c51727b852174dc379a8bd365ae55accf6569
Safei Ashraf <Safei.ashraf@gmail.com>
2020-12-22 12:03:57 +0200

    adding warning msg on input length

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component{
+    state = {searchTerm: ''};
+
 
     render(){
+        let warningMsg;
+        warningMsg = this.state.searchTerm.length < 3? `Search term has to be at least 3 characters long` :'';
         return(<>
         <div className="ui segment">
         <form className="ui form">
             <div className="field">
                 <label>Image Search</label>
-                <input text="search"/>
+                <input 
+                text="search" 
+                onChange={e=>this.setState({searchTerm:e.target.value.toUpperCase()})}
+                value={this.state.searchTerm}
+                />
             </div>
+            <div>{warningMsg}</div>
         </form>    
         </div>    
             </>);
